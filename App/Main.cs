18e056b7907af1ad3e03867c31349c2418257634
Cameron Kirk <cam13469@esri.com>
2023-04-26 10:57:19 +1000

    Finished writing functionality of the WorldNavigation lib

--- App/Main.cs
+++ App/Main.cs
@@ -23,13 +23,18 @@ class App
 
         Debug.WriteLine(c1.DistanceTo(c2));
 
-        Debug.WriteLine(navigator.GetDistanceToClosestMajorCity("Australia", "Melbourne"));
-        Debug.WriteLine(navigator.GetDistanceToClosestMajorCity("France", "Paris"));
-        Debug.WriteLine(navigator.GetDistanceToClosestMajorCity("Nigeria", "Ondo"));
-        Debug.WriteLine(navigator.GetDistanceToClosestMajorCity("Russia", "Moscow"));
+        Debug.WriteLine(navigator.GetDistanceToClosestMajorCityInCountry("Australia", "Melbourne"));
+        Debug.WriteLine(navigator.GetDistanceToClosestMajorCityInCountry("France", "Paris"));
+        Debug.WriteLine(navigator.GetDistanceToClosestMajorCityInCountry("Nigeria", "Ondo"));
+        Debug.WriteLine(navigator.GetDistanceToClosestMajorCityInCountry("Russia", "Moscow"));
 
         Debug.WriteLine(navigator.GetClosestCountry("Australia"));
         Debug.WriteLine(navigator.GetClosestCountry("Rwanda"));
+
+        Debug.WriteLine(navigator.GetClosestCapitalCity("Australia", "Perth"));
+        Debug.WriteLine(navigator.GetClosestCapitalCity("France", "Lyon"));
+        Debug.WriteLine(navigator.GetCapitalCity("Greenland"));
+        Debug.WriteLine(navigator.GetClosestCapitalCity("Germany", "Nuremberg"));
 
     }
 
--- WorldNavigation/Navigator.cs
+++ WorldNavigation/Navigator.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Dynamic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace WorldNavigation
 {
     public class Navigator
@@ -38,9 +31,26 @@ namespace WorldNavigation
             return null;
         }
 
-        public City? GetClosestCapitalCity(string city_name)
+        public Tuple<City, double> GetClosestCapitalCity(string country_name, string city_name)
         {
-            return null;
+            List<City> cities = GetCities(country_name);
+            City home_city = cities.Where(c => c.name == city_name).First();
+
+            List<City> capitals = City.cities_by_id.Values
+                .ToList()
+                .Where(c => (c.name != city_name) && (c.type == "primary"))
+                .ToList();
+
+            Tuple<City, double> closest = new(null, double.MaxValue);
+            foreach (City city in capitals)
+            {
+                if (home_city.DistanceTo(city) < closest.Item2)
+                {
+                    closest = new Tuple<City, double>(city, home_city.DistanceTo(city));
+                }
+            }
+
+            return closest;
         }
 
         public Tuple<Country, double> GetClosestCountry(string country_name)
@@ -63,7 +73,7 @@ namespace WorldNavigation
         }   
 
 
-        public Tuple<City, double> GetDistanceToClosestMajorCity(string country_name, string city_name)
+        public Tuple<City, double> GetDistanceToClosestMajorCityInCountry(string country_name, string city_name)
         {
             List<City> cities = GetMajorCities(country_name);
             City home_city = City.cities_by_name[city_name].Where(city => city.country == country_name).First();
